44ebb7043f8b6714b30dae9bf1dd8dff6a3cd681
SharonObange <obangesharon@gmail.com>
2023-07-24 04:09:26 -0700

    Updated main.h

--- main.h
+++ main.h
@@ -11,11 +11,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
