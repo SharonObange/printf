093395fcf7459389d55db4be156c87d4ad3be611
SharonObange <obangesharon@gmail.com>
2023-07-25 15:00:31 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,55 +3,52 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+
 /**
- * _printf - prints a character and string and handles %
- * @format: pointer to format specifier
- * @...: the variable number of variables
- * Return: the no of characters printed
+ *_printf - prints out characters according to format
+ *@format: character string
+ *@...: unlimited number of arguments
+ *Return: nuber of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list argument;
 	int printed_chars = 0;
+	int character, integer;
+	char *string;
+	float decimal;
 
 	va_start(argument, format);
+	character = va_arg(argument, int);
+	string = va_arg(argument, char *);
+	decimal = va_arg(argument, double);
+	integer = va_arg(argument, int);
+
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == 'c')
+			if (*format == 'd')
 			{
-				int x = va_arg(argument, int);
-
-				_putchar(x);
-				printed_chars = printed_chars + 1;
+				print_int(integer);
 			}
 			else if (*format == 's')
 			{
-				char *x = va_arg(argument, char *);
-
-				while (*x != '\0')
-				{
-
-					_putchar(*x++);
-					printed_chars = printed_chars + 1;
-				}
+				print_str(string);
 			}
-			else
+			else if (*format == 'f')
 			{
-				_putchar('n');
-				printed_chars = printed_chars + 1;
+				print_float(decimal);
+			}
+			else if (*format == 'c')
+			{
+				print_char(character);
 			}
 		}
-		else
-		{
-			_putchar(*format);
-			printed_chars = printed_chars + 1;
-		}
-		printed_chars = printed_chars + 1;
 		format++;
 	}
+
 	va_end(argument);
 	return (printed_chars);
 }
